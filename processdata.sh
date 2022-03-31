b22325c4b6486447d2fc13cfb9871f77777e6601
zzzacwork <zzzacwork@gmail.com>
2022-03-31 17:19:15 +0000

    small fix

--- processdata.sh
+++ processdata.sh
@@ -1,5 +1,10 @@
 source ENV/bin/activate
 
+if ! [ -d 'PLOGS' ]
+then
+  mkdir PLOGS
+fi
+
 while read line; do
   nohup python pymed/process_data.py --articles-dir="$line" &>>PLOGS/"$line".log& 
 done < $1
